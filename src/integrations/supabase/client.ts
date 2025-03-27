d8e316b6327c2284f3da45580c764fc3ae1c8f15
NaurinKhan7 <160589655+NaurinKhan7@users.noreply.github.com>
2025-03-27 18:47:45 +0530

    env

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,13 +1,5 @@
 // Supabase client
 // Learn more: https://supabase.io/docs/guides/with-react
-// import { createClient } from '@supabase/supabase-js';
-// import type { Database } from './types';
-
-// const SUPABASE_URL = "https://wceszevhxwoffvblbwho.supabase.co";
-// const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjZXN6ZXZoeHdvZmZ2Ymxid2hvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5NDA3MDksImV4cCI6MjA1ODUxNjcwOX0.2S90VTZbTKGT3AbzRJpOfe2abyDv4_GcrkHPb2wdgR4";
-
-
-// export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
 
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
